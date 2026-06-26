b7c3cd7632da83337268c7f0d64215cf730668a9
Nidhish Chauhan <solemnsquire@gmail.com>
2026-06-26 13:40:23 +0530

    Debug symbol arm64 linux overlay triplet

--- vcpkg-overlays/triplets/arm64-linux-dynamic-dbg.cmake
+++ vcpkg-overlays/triplets/arm64-linux-dynamic-dbg.cmake
@@ -1,10 +1,17 @@
 set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
+<<<<<<< HEAD
 
+=======
+>>>>>>> e56c51be03 (Debug symbol arm64 linux overlay triplet)
 set(VCPKG_CXX_FLAGS_RELEASE "-g")
 set(VCPKG_C_FLAGS_RELEASE "-g")
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
+<<<<<<< HEAD
 set(VCPKG_FIXUP_ELF_RPATH ON)
+=======
+set(VCPKG_FIXUP_ELF_RPATH ON)
+>>>>>>> e56c51be03 (Debug symbol arm64 linux overlay triplet)
