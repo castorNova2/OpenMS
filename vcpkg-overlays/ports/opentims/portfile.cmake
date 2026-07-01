1d217f171702d7e412c3f53c773575913fcd475f
Nidhish Chauhan <solemnsquire@gmail.com>
2026-07-02 00:26:00 +0530

    Initial overlay port of opentims

---
+++ vcpkg-overlays/ports/opentims/portfile.cmake
@@ -0,0 +1,41 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO michalsta/opentims
+    REF "v${VERSION}"
+    SHA512 43f1b8345a41ac99e4b1ee4c09308f1d9f806bfa91f36c575d52cf7c2757c64877188d65c555c71c357da8dc4efe4b173a81de5b5b0bb0b0ee784b7dd7620a29
+    HEAD_REF master
+)
+
+#Map's the vcpkg library linkage choice onto upstream's BUILD_SHARED_LIBS switch
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+    set(OPENTIMS_BUILD_SHARED ON)
+else()
+    set(OPENTIMS_BUILD_SHARED OFF)
+endif()
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+    -DBUILD_SHARED_LIBS=${OPENTIMS_BUILD_SHARED}
+    -DOPENTIMS_BUILD_LIB=ON
+    -DOPENTIMS_BUILD_PYTHON=OFF
+    -DOPENTIMS_LINK_SQLITE_STATICALLY=ON
+    -DCMAKE_INSTALL_LIBDIR=lib
+    -DCMAKE_INSTALL_BINDIR=bin
+    -DCMAKE_INSTALL_INCLUDEDIR=include
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME opentims CONFIG_PATH lib/cmake/opentims)
+
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/share/doc"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+)
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENCE")
