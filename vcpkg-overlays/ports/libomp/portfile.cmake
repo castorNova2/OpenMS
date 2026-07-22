d1090a4803ba6bcb401c81750c66a7b6c50aad49
Nidhish Chauhan <solemnsquire@gmail.com>
2026-07-22 22:14:35 +0530

    Version change

--- vcpkg-overlays/ports/libomp/portfile.cmake
+++ vcpkg-overlays/ports/libomp/portfile.cmake
@@ -1,13 +1,13 @@
-set(LIBOMP_VERSION "12.0.1")
+set(LIBOMP_VERSION "17.0.6")
 
 vcpkg_download_distfile(ARCHIVE
     URLS
         # OpenMS own host, matches with what contrib already fetches
-        "https://github.com/OpenMS/contrib-sources/releases/download/3.6.0/openmp-${LIBOMP_VERSION}.src.tar.xz"
+        # "https://github.com/OpenMS/contrib-sources/releases/download/3.6.0/openmp-${LIBOMP_VERSION}.src.tar.xz"
         #Upstream as a second source if still online
-        "https://github.com/llvm/llvm-project/releases/download/llvmorg-{LIBOMP_VERSION}/openmp-${LIBOMP_VERSION}.src.tar.xz"
+        "https://github.com/llvm/llvm-project/releases/download/llvmorg-${LIBOMP_VERSION}/openmp-${LIBOMP_VERSION}.src.tar.xz"
     FILENAME "openmp-${LIBOMP_VERSION}.src.tar.xz"
-    SHA512 554edf032995cf80cfb6c878b26510b6c4df09e6bd4813934ea523ff8e121900a91ec59c3d83ee0ba390fb83bcaf6d137f7f6019958b444bdfe6a2b35c1c8d08
+    SHA512 836c48db873e3da64835913c2c1d80efaebdfc7061f153acc7b478ea0d73b9c546a09d70dd1465e3e92684947c4a9cb197886b29cbd42418a778faa56b08cc5e
 )
 
 vcpkg_extract_source_archive(
