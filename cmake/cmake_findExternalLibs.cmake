979371caf89b3d692447fb802adb08dfbeb875a0
Nidhish Chauhan <solemnsquire@gmail.com>
2026-07-30 20:43:00 +0530

    defer call for opentims

--- cmake/cmake_findExternalLibs.cmake
+++ cmake/cmake_findExternalLibs.cmake
@@ -503,9 +503,12 @@ if (WITH_OPENTIMS)
     get_target_property(_opentims_defs opentims::opentims_cpp INTERFACE_COMPILE_DEFINITIONS)
     if(_opentims_defs MATCHES "OPENTIMS_LINK_SQLITE_STATICALLY"
        OR ((_opentims_defs MATCHES "NOTFOUND" OR _opentims_defs STREQUAL "") AND NOT opentims_FOUND))
-      target_include_directories(opentims::opentims_cpp INTERFACE
+      #TODO: Testing
+      cmake_language(DEFER CALL target_include_directories 
+        opentims::opentims_cpp INTERFACE
         "${CMAKE_SOURCE_DIR}/src/openms/extern/SQLiteCpp/sqlite3")
-      target_link_libraries(opentims::opentims_cpp INTERFACE sqlite3)
+      cmake_language(DEFER CALL target_link_libraries 
+        opentims::opentims_cpp INTERFACE sqlite3)
       message(STATUS "opentims: injecting OpenMS sqlite3 (library was built with static sqlite)")
     endif()
   else()
